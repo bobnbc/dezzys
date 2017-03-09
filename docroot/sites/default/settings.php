465355e4e1b5134696cfbb8ce3b16f8c03d971e2
Bob Liu <bob.liu@nbcuni.com>
2017-03-09 14:30:24 -0500

    testing settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -244,7 +244,7 @@
  *   );
  * @endcode
  */
-
+/*
 if(($_SERVER['HTTP_HOST']) == "localhost"){
 
 	$databases = array (
@@ -275,7 +275,8 @@ else
 
 }
 
-
+*/
+$databases = array();
 /**
  * Access control for update.php script.
  *
@@ -664,4 +665,10 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 // database in each site environment (Dev, Stage, or Prod). To use this
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
+
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/dezzysfootwear/dezzysfootwear-settings.inc');
+}
+
+print_r($_ENV);
 
